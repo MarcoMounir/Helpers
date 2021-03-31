6ad371a55b706d93765198f5055d2cf9c839a890
Marco Mounir <marco.mounir.fcis@gmail.com>
2021-03-31 16:32:21 +0200

    Refactor by removing duplicate code (Sql connection init, sql command execution and sqldatareader reading logic)

--- SqlDataReaderMapper/DatabaseConnector.cs
+++ SqlDataReaderMapper/DatabaseConnector.cs
@@ -15,56 +15,24 @@ namespace SqlDataReaderMapper
             _connectionString = connectionString;
             _commandTimeout = commandTimeout;
         }
+
         public IEnumerable<TReturnType> ReadDataFromStoredProcedure<TReturnType>(string storedProcedureName, IEnumerable<SqlParameter> sqlParameters = null) where TReturnType : class, new()
         {
-            List<TReturnType> result = new List<TReturnType>();
-            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
-            {
-                sqlConnection.Open();
-                using (SqlCommand sqlCommand = new SqlCommand(storedProcedureName, sqlConnection))
-                {
-                    sqlCommand.CommandTimeout = _commandTimeout;
-                    sqlCommand.CommandType = CommandType.StoredProcedure;
-                    if(sqlParameters != null)
-                        sqlCommand.Parameters.AddRange(sqlParameters.ToArray());
-                    using (SqlDataReader reader = sqlCommand.ExecuteReader())
-                    {
-                        SqlDataReaderMapper<TReturnType> sqlMapper = new SqlDataReaderMapper<TReturnType>(reader);
-                        while (reader.Read())
-                        {
-                            TReturnType returnObject = sqlMapper.Map();
-                            result.Add(returnObject);
-                        }
-                    }
-                }
-            }
-            return result;
+            return Execute<TReturnType>(CommandType.StoredProcedure, storedProcedureName, sqlParameters);
         }
         public IEnumerable<TReturnType> ReadDataFromStoredProcedure<TReturnType>(string storedProcedureName, Func<SqlDataReader, TReturnType> mapDataAction, IEnumerable<SqlParameter> sqlParameters = null) where TReturnType : class, new()
         {
-            List<TReturnType> result = new List<TReturnType>();
-            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
-            {
-                sqlConnection.Open();
-                using (SqlCommand sqlCommand = new SqlCommand(storedProcedureName, sqlConnection))
-                {
-                    sqlCommand.CommandTimeout = _commandTimeout;
-                    sqlCommand.CommandType = CommandType.StoredProcedure;
-                    if (sqlParameters != null)
-                        sqlCommand.Parameters.AddRange(sqlParameters.ToArray());
-                    using (SqlDataReader reader = sqlCommand.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            TReturnType returnObject = mapDataAction(reader);
-                            result.Add(returnObject);
-                        }
-                    }
-                }
-            }
-            return result;
+            return Execute<TReturnType>(CommandType.StoredProcedure, storedProcedureName, sqlParameters, mapDataAction);
         }
         public IEnumerable<TReturnType> ReadDataFromSqlCommand<TReturnType>(string commandText, IEnumerable<SqlParameter> sqlParameters = null) where TReturnType : new()
+        {
+            return Execute<TReturnType>(CommandType.Text, commandText, sqlParameters);
+        }
+        public IEnumerable<TReturnType> ReadDataFromSqlCommand<TReturnType>(string commandText, Func<SqlDataReader, TReturnType> mapDataAction, IEnumerable<SqlParameter> sqlParameters=null) where TReturnType : class, new()
+        {
+            return Execute<TReturnType>(CommandType.Text, commandText, sqlParameters, mapDataAction);
+        }
+        private IEnumerable<TReturnType> Execute<TReturnType>(CommandType commandType, string commandText, IEnumerable<SqlParameter> sqlParameters = null, Func<SqlDataReader, TReturnType> mapDataAction = null) where TReturnType : new()
         {
             List<TReturnType> result = new List<TReturnType>();
             using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
@@ -73,6 +41,7 @@ namespace SqlDataReaderMapper
                 using (SqlCommand sqlCommand = new SqlCommand(commandText, sqlConnection))
                 {
                     sqlCommand.CommandTimeout = _commandTimeout;
+                    sqlCommand.CommandType = commandType;
                     if (sqlParameters != null)
                         sqlCommand.Parameters.AddRange(sqlParameters.ToArray());
                     using (SqlDataReader reader = sqlCommand.ExecuteReader())
@@ -80,30 +49,7 @@ namespace SqlDataReaderMapper
                         SqlDataReaderMapper<TReturnType> sqlMapper = new SqlDataReaderMapper<TReturnType>(reader);
                         while (reader.Read())
                         {
-                            TReturnType returnObject = sqlMapper.Map();
-                            result.Add(returnObject);
-                        }
-                    }
-                }
-            }
-            return result;
-        }
-        public IEnumerable<TReturnType> ReadDataFromSqlCommand<TReturnType>(string commandText, Func<SqlDataReader, TReturnType> mapDataAction, IEnumerable<SqlParameter> sqlParameters=null) where TReturnType : class, new()
-        {
-            List<TReturnType> result = new List<TReturnType>();
-            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
-            {
-                sqlConnection.Open();
-                using (SqlCommand sqlCommand = new SqlCommand(commandText, sqlConnection))
-                {
-                    sqlCommand.CommandTimeout = _commandTimeout;
-                    if (sqlParameters != null)
-                        sqlCommand.Parameters.AddRange(sqlParameters.ToArray());
-                    using (SqlDataReader reader = sqlCommand.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            TReturnType returnObject = mapDataAction(reader);
+                            TReturnType returnObject = mapDataAction == null ? sqlMapper.Map() : mapDataAction(reader);
                             result.Add(returnObject);
                         }
                     }
